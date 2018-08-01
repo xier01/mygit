6f30cbb3d569cf7302f3d1788b9c2940c21e6864
xier01 <41987204+xier01@users.noreply.github.com>
2018-08-01 19:26:54 +0800

    Update HelloWold.java
    提交了，改变了

--- sys/src/main/java/sys/HelloWold.java
+++ sys/src/main/java/sys/HelloWold.java
@@ -4,6 +4,7 @@ public class HelloWold {
          //234r
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(345);
 		System.out.println(1253);
 	}
 
